eff48674bd3400a7ca978948bbf3f34c48ba2371
Nextware Digital <abhishek.ai.learn@gmail.com>
2025-11-29 10:25:37 +0530

    Add Cors Frontend

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 // CORS (More secure)
 app.use(
   cors({
-    origin: true,
+    origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization",
